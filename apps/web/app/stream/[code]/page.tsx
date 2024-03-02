ecc49460698a38482d5ce55560545edf692d2310
AngaBlue <contact@anga.blue>
2024-03-02 23:06:43 +1100

    feat: frame streaming and clicking

--- apps/web/app/stream/[code]/page.tsx
+++ apps/web/app/stream/[code]/page.tsx
@@ -13,28 +13,39 @@ interface StreamPageProps {
     };
 }
 
+type Vec3 = [number, number, number];
+interface Frame {
+    image: Buffer;
+    location: Vec3;
+    direction: Vec3;
+}
 const emojis = ['❗', '⬇️', '✋', '🔥'];
+
+const socket = io(process.env.NEXT_PUBLIC_API_HOST);
 
 export default function StreamPage({ params: { code } }: StreamPageProps) {
     const [loading, setLoading] = useState(true);
     const [currentEmoji, setCurrentEmoji] = useState('');
+    const [frame, setFrame] = useState<Frame | null>(null);
 
     useEffect(() => {
-        console.log('Connecting to socket');
-
-        const socket = io(process.env.NEXT_PUBLIC_API_HOST, {
-            extraHeaders: {
-                // Send the code as a bearer token
-                Authorization: `Bearer ${code}`
-            }
-        });
-
-        socket.on('connect', () => {
+        async function connect() {
             console.log('Connected to socket server');
+            const subscribed = await socket.emitWithAck('subscribe', code);
+            if (!subscribed) {
+                console.error('Failed to subscribe to stream');
+                return;
+            }
             setLoading(false);
+        }
 
-            // Identify as a control socket
-            socket.send('identify', 'control');
+        connect();
+
+        socket.on('frame', (image: Buffer, location: Vec3, direction: Vec3) => {
+            console.log('Received frame', image, location, direction);
+
+            // Only update the frame if we don't have an emoji selected
+            if (!currentEmoji) setFrame({ image, location, direction });
         });
 
         // Clean up
@@ -49,6 +60,18 @@ export default function StreamPage({ params: { code } }: StreamPageProps) {
         setCurrentEmoji(emoji);
     }
 
+    function onFrameClick(e: React.MouseEvent<HTMLImageElement>) {
+        if (!currentEmoji || !frame) return;
+        setCurrentEmoji('');
+
+        // Calculate the percentage of the image the click was at
+        const x = e.nativeEvent.offsetX / e.currentTarget.clientWidth;
+        const y = e.nativeEvent.offsetY / e.currentTarget.clientHeight;
+
+        // Emit position
+        socket.emit('click', { x, y, location: frame.location, direction: frame.direction, emoji: currentEmoji });
+    }
+
     return (
         <div className='flex justify-center items-center p-6 gap-8'>
             <div className='flex flex-col gap-4'>
@@ -56,6 +79,9 @@ export default function StreamPage({ params: { code } }: StreamPageProps) {
                     className='aspect-square w-[80vh] max-w-ful rounded-2xl relative overflow-hidden'
                     style={currentEmoji ? cursor(currentEmoji) : {}}
                 >
+                    {/* Image */}
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    {frame && <img src={`data:image/jpg;base64,${frame.image.toString('base64')}`} alt='Stream' onClick={onFrameClick} />}
                     {/* Loading background */}
                     <div className={cn('absolute top-0 left-0 w-full h-full bg-slate-200 flex justify-center items-center')}>
                         {loading && <FaSpinner className='w-16 h-16 text-primary animate-spin' />}
