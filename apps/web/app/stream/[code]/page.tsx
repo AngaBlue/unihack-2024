361aba8e3a554f6f512219478e4a3056a13e35ec
AngaBlue <contact@anga.blue>
2024-03-02 14:53:33 +1100

    feat: connect to socket using code

--- apps/web/app/stream/[code]/page.tsx
+++ apps/web/app/stream/[code]/page.tsx
@@ -1,4 +1,5 @@
-import '@web/lib/socket';
+import { io } from 'socket.io-client';
+import { useEffect } from 'react';
 
 interface StreamPageProps {
     params: {
@@ -7,6 +8,31 @@ interface StreamPageProps {
 }
 
 export default function StreamPage({ params: { code } }: StreamPageProps) {
+    useEffect(() => {
+        console.log('Connecting to socket');
+
+        const socket = io(process.env.NEXT_PUBLIC_API_HOST, {
+            extraHeaders: {
+                // Send the code as a bearer token
+                Authorization: `Bearer ${code}`
+            }
+        });
+
+        socket.on('connect', () => {
+            console.log('Connected to socket server');
+
+            // Identify as a control socket
+            socket.send('identify', 'control');
+        });
+
+        // Clean up
+        return () => {
+            console.log('Disconnecting from socket');
+            socket.off('connect');
+            socket.disconnect();
+        };
+    }, []);
+
     return (
         <div>
             <h1>Code Parameter</h1>
