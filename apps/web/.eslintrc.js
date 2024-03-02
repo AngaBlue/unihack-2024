53cac190903bef28d2f4be5a10acd258e6a302b1
AngaBlue <contact@anga.blue>
2024-03-02 21:57:02 +1100

    style: disable extraneous deps warning

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
         ...eslintConfigNext.rules,
         'global-require': 'off',
         // 'no-html-link-for-pages': [2, path.join(__dirname, 'app')],
+        'import/no-extraneous-dependencies': 'off',
         '@next/next/no-html-link-for-pages': [2, path.join(__dirname, 'app')]
     },
     parserOptions: {
