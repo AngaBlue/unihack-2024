11a2b13154d2cb56b2fddce7ed7e76b80e0d1ba6
AngaBlue <contact@anga.blue>
2024-03-02 13:48:46 +1100

    fix: revert naming change

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
     darkMode: ['class'],
-    content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './Components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+    content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
     prefix: '',
     theme: {
         container: {
