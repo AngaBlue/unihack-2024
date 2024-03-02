aa8d89ce40efded2f7e9a9d91c802f90477f2eb8
AngaBlue <contact@anga.blue>
2024-03-02 13:23:21 +1100

    fix: use next head

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
     return (
         <html lang='en'>
-            <head>
+            <Head>
                 <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
                 <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
                 <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
@@ -20,7 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
                 <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#112c4a' />
                 <meta name='msapplication-TileColor' content='#112c4a' />
                 <meta name='theme-color' content='#112c4a' />
-            </head>
+            </Head>
             <body className={inter.className}>{children}</body>
         </html>
     );
