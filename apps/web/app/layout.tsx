6b7207134ee1b0da8ef9fe03e1ac3b7be7596a47
AngaBlue <contact@anga.blue>
2024-03-03 01:21:01 +1100

    fix: opaque footer

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
                     <main className='flex-1'>{children}</main>
                 </div>
                 {/* Footer */}
-                <footer className='flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t'>
+                <footer className='flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-white'>
                     <p className='text-xs text-gray-500 dark:text-gray-400'>© 2024 InstructAR. All rights reserved.</p>
                     <nav className='sm:ml-auto flex gap-4 sm:gap-6'>
                         <Link className='text-xs hover:underline underline-offset-4' href='#'>
